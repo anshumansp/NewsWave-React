82d0e5582d85d2178e625e71f8712432eddeb5a3
Anshuman Parmar <anshumansp16@gmail.com>
2023-09-01 14:00:58 +0530

    Improved About Section

--- src/components/About.js
+++ src/components/About.js
@@ -5,8 +5,8 @@ export class About extends Component {
     return (
       <div>
         <section className="text-gray-600 body-font">
-          <div className="container px-5 py-10 mt-5 mx-auto flex flex-col">
-            <div className="lg:w-4/6 mx-auto">
+          <div className="container px-5 py-8 mt-6 mx-auto flex flex-col">
+            <div className="lg:w-4/6 mt-10 mx-auto">
               <div className="rounded-lg h-64 overflow-hidden">
                 <img
                   alt="content"
@@ -14,7 +14,7 @@ export class About extends Component {
                   src="https://images.unsplash.com/photo-1582266255765-fa5cf1a1d501?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
                 />
               </div>
-              <div className="flex flex-col sm:flex-row mt-20">
+              <div className="flex flex-col sm:flex-row mt-10">
                 <div className="sm:w-1/3 text-center sm:pr-8 sm:py-8">
                   <div className="w-20 h-20 inline-flex items-center justify-center text-gray-400">
                     <img
@@ -37,8 +37,8 @@ export class About extends Component {
                     </p>
                   </div>
                 </div>
-                <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
-                  <p className="leading-relaxed text-lg mt-8">
+                <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-0 pt-0 sm:mt-0 text-center sm:text-left">
+                  <p className="leading-relaxed text-lg my-0">
                     Welcome to NewsWave! Your trusted source for the latest and
                     most accurate news from around the world. With a commitment
                     to delivering real news from reputable sources, we cover a
@@ -49,6 +49,7 @@ export class About extends Component {
                     what's happening globally. Trust NewsWave to bring you the
                     news that matters, from the most reliable sources.
                   </p>
+                  <p className="leading-relaxed text-lg mt-8">All Rights Reserved | © 2023 - Anshuman Parmar</p>
                 </div>
               </div>
             </div>
